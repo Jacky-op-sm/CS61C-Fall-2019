11cd59f65c7296d585562d8b46d9c636b30be7b0
Jacky Lin <jackylin@Jackys-iMac.local>
2021-05-31 14:45:47 +0800

    add hand-written control design help

---
+++ proj3/my_test_in_assembly/I_type.s
@@ -0,0 +1,15 @@
+addi t0, x0, 1
+addi t1, x0, 2
+addi t2, x0, -3
+addi s0, x0, 4
+addi s1, x0, 5
+slli t0, s1, 4
+slti t0, t1, 3
+slti t0, t1, 2
+slti t0, t1, 1
+xori t0, s1, 2
+srli t0, s1, 2
+srai t0, t2, 2
+ori t0, t2, 2
+andi t0, t2, 7
+
